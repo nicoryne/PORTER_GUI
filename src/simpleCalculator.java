9f740d0e49f1e5106c22459729fe113050969a78
LAB12_USER ON WS06 <L12X14W06@citulab.net>
2023-11-17 15:31:14 +0800

    Added Simple Calculator - Nicolo

---
+++ src/simpleCalculator.java
@@ -0,0 +1,55 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class simpleCalculator extends JFrame {
+    private JButton btnCompute;
+    private JTextField tfNumber1;
+    private JComboBox cbOperations;
+    private JTextField tfNumber2;
+    private JPanel panelCalculator;
+    private JTextField label1stNumber;
+    private JLabel tf1stNumber;
+    private JLabel label2ndNumber;
+    private JLabel labelResult;
+
+    public simpleCalculator() {
+
+
+        btnCompute.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                double first_num = Double.parseDouble(tfNumber1.getText());
+                double second_num = Double.parseDouble(tfNumber2.getText());
+                if(cbOperations.getSelectedIndex() == 0) {
+                    //ADDITION
+                    label1stNumber.setText(String.valueOf(first_num + second_num));
+                } else if(cbOperations.getSelectedIndex() == 1) {
+                    //SUBTRACTION
+                    label1stNumber.setText(String.valueOf(first_num - second_num));
+                } else if(cbOperations.getSelectedIndex() == 2) {
+                    //MULTIPLICATION
+                    label1stNumber.setText(String.valueOf(first_num * second_num));
+                } else if(cbOperations.getSelectedIndex() == 3) {
+                    //DIVISION
+                    if(second_num == 0) {
+                        JOptionPane.showMessageDialog(panelCalculator, "Invalid divisor");
+                    } else {
+                        label1stNumber.setText(String.valueOf(first_num / second_num));
+                    }
+                } else {
+                    JOptionPane.showMessageDialog(panelCalculator, "Please select a valid operation!");
+                }
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        simpleCalculator app = new simpleCalculator();
+        app.setContentPane(app.panelCalculator);
+        app.setSize(800, 200);
+        app.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        app.setTitle("Simple Calculator");
+        app.setVisible(true);
+    }
+}
