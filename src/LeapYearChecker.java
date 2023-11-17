b23f512019be2d20007be08e56b4f45a13b59969
LAB12_USER ON WS06 <L12X14W06@citulab.net>
2023-11-17 14:56:36 +0800

    Added Leap Year Checker - Nicolo

---
+++ src/LeapYearChecker.java
@@ -0,0 +1,37 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+public class LeapYearChecker extends JFrame{
+
+    private JButton btnCheckYear;
+    private JTextField tfYear;
+    private JPanel panelLeapYear;
+
+
+    public LeapYearChecker() {
+        btnCheckYear.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int year = Integer.parseInt(tfYear.getText());
+                if(year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+                    JOptionPane.showMessageDialog(panelLeapYear, "Leap year");
+                } else {
+                    JOptionPane.showMessageDialog(panelLeapYear, "Not a leap year");
+
+                }
+
+
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        LeapYearChecker app = new LeapYearChecker();
+        app.setContentPane(app.panelLeapYear);
+        app.setSize(400, 200);
+        app.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        app.setTitle("Leap Year Checker");
+        app.setVisible(true);
+    }
+
+}
